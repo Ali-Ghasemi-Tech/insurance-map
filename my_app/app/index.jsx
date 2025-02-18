8689e49dad150d9d28fb3132e7619fa8d42a8e62
Ali-Ghasemi-Tech <thelastblade30@gmail.com>
2025-02-18 20:34:52 +0330

    moving back to windows

--- my_app/app/index.jsx
+++ my_app/app/index.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { StyleSheet,Text, View, Pressable } from "react-native";
 import SearchableDropdown from "react-native-searchable-dropdown";
 import { useNavigation } from '@react-navigation/native'; 
@@ -9,12 +8,7 @@ import { Link } from 'expo-router';
 
 import React, { Component, Fragment } from 'react';
 
-=======
-import { StyleSheet,Text, View , Button } from "react-native";
-import SearchableDropdown from "react-native-searchable-dropdown";
 
-import React, { Component, Fragment , usestate } from 'react';
->>>>>>> 413835119aef642f2d7c49c7a8f23f2c71312c32
 
 var items = [
   {
@@ -86,7 +80,6 @@ var items = [
     name: 'پاسارگاد',
   },
 ];
-<<<<<<< HEAD
 
 
 function App() {
@@ -195,94 +188,6 @@ function App() {
 }
 
 
-=======
-class App extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectItem:
-        {
-          id: 0,
-          name: 'هیچکدام',
-        },
-      
-    }
-  }
-  render() {
-  return (
-        <Fragment>
-          <Text style={styles.header}>با انتخاب بیمه ای که داری میتونی بیمارستانایی که نزدیکتن و بیمه تو هم پوشش میدن پیدا کنی</Text>
-          <View style={styles.container}>
-          <SearchableDropdown
-            onItemSelect={(item) => {
-              let items = this.state.selectItem;
-              items = item
-              this.setState({ selectItem: items });
-            }}
-            containerStyle={{ padding: 5 }}
-            onRemoveItem={(item, index) => {
-              const items = this.state.selectItem.filter((sitem) => sitem.id !== item.id);
-              this.setState({ selectItem: items });
-            }}
-            itemStyle={{
-              padding: 10,
-              marginTop: 2,
-              backgroundColor: '#ddd',
-              borderColor: '#bbb',
-              borderWidth: 1,
-              borderRadius: 5,
-            }}
-            itemTextStyle={{ color: '#222'  , direction: 'rtl'}}
-            itemsContainerStyle={{ maxHeight: 140 }}
-            items={items}
-            defaultIndex={2}
-            resetValue={false}
-            textInputProps={
-              {
-                placeholder: this.state.selectItem.name,
-                underlineColorAndroid: "transparent",
-                style: {
-                    direction: 'rtl',
-                    padding: 12,
-                    borderWidth: 1,
-                    borderColor: '#ccc',
-                    borderRadius: 5,
-                },
-              }
-            }
-            listProps={
-              {
-                nestedScrollEnabled: true,
-              }
-            }
-        />
-
-        <Button title="جستجو" onPress={() => handleApi(this.state.selectItem.id)}/>
-        </View>
-      </Fragment>
-  );
-  }
-}
-
-const handleApi = (id)=>{
-  console.log(id)
-}
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 50,
-    display: 'flex',
-    flexDirection: 'column',
-    gap: 10,
-  },
-  header:{
-    fontSize: 20,
-    direction: 'rtl'
-  }
-
-  
-});
->>>>>>> 413835119aef642f2d7c49c7a8f23f2c71312c32
 
 export default App
